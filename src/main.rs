12558a187f246ea2e434dc302b385ecac06388f5
myyrakle <sssang97@naver.com>
2023-01-17 01:12:32 +0900

    feat: health check api

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,17 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
+// basic handler that responds with a static string
+async fn health() -> &'static str {
+    "OK"
+}
+
 #[tokio::main]
 async fn main() -> Result<(), LambdaError> {
     // build our application with a route
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health));
 
     if is_running_on_lambda() {
         // Run app on AWS Lambda
@@ -18,7 +25,9 @@ async fn main() -> Result<(), LambdaError> {
     } else {
         // Run app on local server
         let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-        axum::Server::bind(&addr).serve(app.into_make_service()).await?;
+        axum::Server::bind(&addr)
+            .serve(app.into_make_service())
+            .await?;
     }
     Ok(())
 }
