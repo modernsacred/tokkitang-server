bec7a06d16d5d171fece8e5c0c091b7497f4077c
myyrakle <sssang97@naver.com>
2023-01-28 20:23:20 +0900

    add: team model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,2 +1,5 @@
 pub mod user;
 pub use user::*;
+
+pub mod team;
+pub use team::*;
---
+++ src/models/team.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+use aws_sdk_dynamodb::model::AttributeValue;
+use serde::{Deserialize, Serialize};
+
+// 팀 모델
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Team {
+    pub id: String,
+    pub name: String,
+    pub description: String,
+    pub owner_id: String,
+    pub thumbnail_url: Option<String>,
+}
+
+impl Team {
+    pub const NAME: &'static str = "modeler_team";
+
+    pub fn to_hashmap(&self) -> Option<HashMap<String, AttributeValue>> {
+        let mut map = HashMap::new();
+        map.insert("id".to_string(), AttributeValue::S(self.id.to_owned()));
+        map.insert("name".to_string(), AttributeValue::S(self.name.to_owned()));
+        map.insert(
+            "description".to_string(),
+            AttributeValue::S(self.description.to_owned()),
+        );
+        map.insert(
+            "owner_id".to_string(),
+            AttributeValue::S(self.owner_id.to_owned()),
+        );
+
+        if let Some(thumbnail_url) = self.thumbnail_url.clone() {
+            map.insert(
+                "thumbnail_url".to_string(),
+                AttributeValue::S(thumbnail_url),
+            );
+        }
+
+        Some(map)
+    }
+
+    pub fn from_hashmap(hashmap: Option<&HashMap<String, AttributeValue>>) -> Option<Self> {
+        let id = hashmap?.get("id")?.as_s().ok()?.to_owned();
+        let name = hashmap?.get("name")?.as_s().ok()?.to_owned();
+        let description = hashmap?.get("description")?.as_s().ok()?.to_owned();
+        let owner_id = hashmap?.get("owner_id")?.as_s().ok()?.to_owned();
+        let thumbnail_url = hashmap?
+            .get("thumbnail_url")?
+            .as_s()
+            .ok()
+            .map(|e| e.to_owned());
+
+        Some(Team {
+            id: id.to_owned(),
+            name,
+            description,
+            owner_id,
+            thumbnail_url,
+        })
+    }
+}
