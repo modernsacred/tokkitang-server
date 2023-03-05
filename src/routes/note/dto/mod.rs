06e9e997d7dbc89a4b4bd38a3cb4e6faeecdf81c
myyrakle <sssang97@naver.com>
2023-03-06 04:26:26 +0900

    feat: update note API

--- src/routes/note/dto/mod.rs
+++ src/routes/note/dto/mod.rs
@@ -1,7 +1,11 @@
 pub mod create_note_request;
 pub mod create_note_response;
 pub mod get_note_response;
+pub mod update_note_request;
+pub mod update_note_response;
 
 pub use create_note_request::CreateNoteRequest;
 pub use create_note_response::CreateNoteResponse;
 pub use get_note_response::*;
+pub use update_note_request::UpdateNoteRequest;
+pub use update_note_response::UpdateNoteResponse;
--- src/routes/note/handler.rs
+++ src/routes/note/handler.rs
@@ -19,13 +19,17 @@ use crate::{
 };
 
 use super::{
-    dto::{CreateNoteRequest, CreateNoteResponse, GetNoteItem, GetNoteResponse},
+    dto::{
+        CreateNoteRequest, CreateNoteResponse, GetNoteItem, GetNoteResponse, UpdateNoteRequest,
+        UpdateNoteResponse,
+    },
     NoteService,
 };
 
 pub async fn router() -> Router {
     let app = Router::new()
         .route("/", post(create_note))
+        .route("/:note_id", put(update_note))
         .route("/:note_id", get(get_note));
 
     app
@@ -103,6 +107,89 @@ async fn create_note(
     match note_service.create_note(data).await {
         Ok(note_id) => {
             response.note_id = note_id;
+            response.success = true;
+        }
+        Err(error) => {
+            println!("error: {:?}", error);
+            return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        }
+    }
+
+    Json(response).into_response()
+}
+
+async fn update_note(
+    current_user: Extension<CurrentUser>,
+    database: Extension<Arc<Client>>,
+    Path(note_id): Path<String>,
+    Json(body): Json<UpdateNoteRequest>,
+) -> impl IntoResponse {
+    let user = if let Some(user) = current_user.user.clone() {
+        user
+    } else {
+        return (StatusCode::UNAUTHORIZED).into_response();
+    };
+
+    let team_service = TeamService::new(database.clone());
+    let note_service = NoteService::new(database.clone());
+    let project_service = ProjectService::new(database.clone());
+
+    let mut response = UpdateNoteResponse { success: false };
+
+    let project_id = match note_service.get_note_by_id(note_id.clone()).await {
+        Ok(note) => note.project_id,
+        Err(error) => {
+            println!("error: {:?}", error);
+            return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        }
+    };
+
+    let team_id = match project_service.get_project_by_id(project_id.clone()).await {
+        Ok(project) => project.team_id,
+        Err(error) => {
+            if let AllError::NotFound = error {
+                println!("# 프로젝트 없음");
+                return (StatusCode::FORBIDDEN).into_response();
+            } else {
+                println!("error: {:?}", error);
+                return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+            }
+        }
+    };
+
+    match team_service
+        .find_team_user_by_team_and_user_id(team_id, user.id)
+        .await
+    {
+        Ok(Some(team_user)) => match team_user.authority {
+            TeamUserAuthority::Owner | TeamUserAuthority::Admin | TeamUserAuthority::Write => {
+                println!("# 권한 허용: OWNER OR ADMIN OR WRITE");
+            }
+            _ => {
+                println!("# 권한 부족: NEED WRITE");
+                return (StatusCode::FORBIDDEN).into_response();
+            }
+        },
+        Ok(None) => {
+            println!("# 권한 부족: NOT TEAM MEMBER");
+            return (StatusCode::FORBIDDEN).into_response();
+        }
+        Err(error) => {
+            println!("error: {:?}", error);
+            return (StatusCode::INTERNAL_SERVER_ERROR).into_response();
+        }
+    }
+
+    let data = Note {
+        id: note_id,
+        project_id: project_id,
+        content: body.content.clone(),
+        x: body.x,
+        y: body.y,
+    };
+
+    match note_service.create_note(data).await {
+        Ok(_) => {
             response.success = true;
         }
         Err(error) => {
