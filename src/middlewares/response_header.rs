62809b84ccef1eb5c59b7aba5acea5a69745877c
myyrakle <sssang97@naver.com>
2023-03-11 04:57:25 +0900

    fix: cors setting

--- src/middlewares/response_header.rs
+++ src/middlewares/response_header.rs
@@ -1,16 +1,15 @@
 use axum::{http::Request, middleware::Next, response::Response};
 
 pub async fn response_header_middleware<B>(req: Request<B>, next: Next<B>) -> Response {
+    let origin = req.headers().get("Origin").map(|o| o.to_owned());
+
     let mut response = next.run(req).await;
 
-    let headers = response.headers_mut();
+    if let Some(origin) = origin {
+        let response_headers = response.headers_mut();
 
-    headers.insert(
-        "Access-Control-Allow-Origin",
-        "https://tokkitang.com; http://localhost:5173"
-            .parse()
-            .unwrap(),
-    );
+        response_headers.insert("Access-Control-Allow-Origin", origin);
+    }
 
     response
 }
