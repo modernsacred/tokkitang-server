6fba09ddda5d1544b526264e117dd39047b4da70
myyrakle <sssang97@naver.com>
2023-03-12 00:19:57 +0900

    rollback: cors setting

--- src/middlewares/response_header.rs
+++ src/middlewares/response_header.rs
@@ -1,17 +1,17 @@
 use axum::{http::Request, middleware::Next, response::Response};
 
 pub async fn response_header_middleware<B>(req: Request<B>, next: Next<B>) -> Response {
-    // let origin = req.headers().get("Origin").map(|o| o.to_owned());
+    let origin = req.headers().get("Origin").map(|o| o.to_owned());
 
-    let response = next.run(req).await;
+    let mut response = next.run(req).await;
 
-    // if let Some(origin) = origin {
-    //     let response_headers = response.headers_mut();
+    if let Some(origin) = origin {
+        let response_headers = response.headers_mut();
 
-    //     if !response_headers.contains_key("Access-Control-Allow-Origin") {
-    //         response_headers.insert("Access-Control-Allow-Origin", origin);
-    //     }
-    // }
+        if !response_headers.contains_key("Access-Control-Allow-Origin") {
+            response_headers.insert("Access-Control-Allow-Origin", origin);
+        }
+    }
 
     response
 }
